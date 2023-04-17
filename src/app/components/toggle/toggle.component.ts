0a4c7f748f650dc6587197635f11049af2d70e24
tamilvanan31 <tamilvananb31@gmail.com>
2023-04-17 10:17:53 +0530

    Add icon

--- src/app/components/toggle/toggle.component.ts
+++ src/app/components/toggle/toggle.component.ts
@@ -1,23 +1,56 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-toggle',
   templateUrl: './toggle.component.html',
   styleUrls: ['./toggle.component.scss']
-})
-export class ToggleComponent implements OnInit {
+})export class ToggleComponent implements AfterViewInit {
 
-@Input() value = false;
-@Output() toggleChange: EventEmitter<boolean> = new EventEmitter<boolean>;
-  constructor() { 
-    
+  constructor(private renderer: Renderer2) {}
+
+  @ViewChild('doneSwitch', { read: ElementRef }) element: ElementRef | undefined;
+  @Input() value = false;
+  @Output() toggleChange: EventEmitter<boolean> = new EventEmitter<boolean>;
+
+  checked = false;
+  disabled = false;
+
+  ngAfterViewInit() {
+    this.setIcon();
   }
 
-  ngOnInit(): void {
+  setIcon() {
+    if (this.element) {
+      const targetSpan: HTMLElement = this.element.nativeElement.querySelector('.mat-slide-toggle-thumb');
+      while (targetSpan.firstChild) {
+        targetSpan.firstChild.remove();
+      }
+      const elem = this.renderer.createElement('mat-icon');
+      const icon = this.checked ? 'done' : 'remove';
+      elem.setAttribute('class', 'mat-icon notranslate material-icons mat-icon-no-color light-mode-switch-icon');
+      elem.textContent = icon
+      targetSpan.appendChild(elem);
+    }
+  }
+
+  changeTheme(event: any) {
+    this.checked = !this.checked;
+    this.setIcon();
+    this.toggleChange.emit(event.checked)
   }
 
   toggle(event: any) {
     console.log(event)
-    this.toggleChange.emit(event.checked)
-    }
+
+  }
 }
